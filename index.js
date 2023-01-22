95b2d79c6de6d1c044b61959f8bd3b24e246ce4e
Indrajit Pal <palindrajit10@gmail.com>
2023-01-22 18:59:14 +0530

    .

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ mongoose.connect(DB, {
 server.prependListener("request", (req, res) => {
    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 });
 
 const server = app.listen(process.env.PORT, () => {
