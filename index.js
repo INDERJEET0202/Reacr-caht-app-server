8453f0428c477cae9cf227fda15cbd042cac6759
Indrajit Pal <palindrajit10@gmail.com>
2023-01-22 18:57:22 +0530

    header added

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ mongoose.connect(DB, {
 
 server.prependListener("request", (req, res) => {
    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 });
 
 const server = app.listen(process.env.PORT, () => {
