03b32fd464f8c5c68004fde05399d68c2a7be83d
Indrajit Pal <palindrajit10@gmail.com>
2023-07-14 15:44:05 +0530

    frontend as vercel is updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const DB = process.env.DB;
 
 const socket = require("socket.io");
 
-app.use(cors({origin: 'https://chatz-xyz.netlify.app'}));
+app.use(cors({ origin: 'https://chatz-xyz.netlify.app' }));
 app.use(express.json())
 
 app.use("/api/auth", userRoutes);
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
 
 const io = socket(server, {
     cors: {
-        origin: "https://chatz-xyz.netlify.app",
+        origin: "chat-and-react.vercel.app",
         credentials: true,
     },
 });
